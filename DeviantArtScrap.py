3ec649150bcff34ca9fce82a603a68b37f95f8fb
yizongk <yizongk@gmail.com>
2020-06-24 01:24:00 -0400

    feat: Add ability to read in config json file to set download location etc

--- DeviantArtScrap.py
+++ DeviantArtScrap.py
@@ -3,9 +3,49 @@ from bs4 import BeautifulSoup
 import re
 from tqdm import tqdm
 import math
+import json
+
+# Return the dict of the json file
+def decode_json_config_file_to_dict( filepath='' ):
+    if filepath == '':
+        print('ERROR: decode_json_config_file_to_dict(): Arg filepath is empty string')
+        return None
+
+    try:
+        f = open(filepath)
+        data = json.load(f)
+        return data
+    except Exception as e:
+        print('ERROR: decode_json_config_file_to_dict(): Fail to open {}'.format(filepath), e)
+        return None
+
+    return None
+
+# Only works for first level of variables, for now, until multiple level is implemented
+def get_single_variable_from_json_file( filepath='', arg_name='' ):
+    if filepath == '':
+        print('ERROR: get_single_variable_from_json_file(): Arg filepath is empty string')
+        return None
+
+    if arg_name == '':
+        print('ERROR: get_single_variable_from_json_file(): Arg arg_name is empty string')
+        return None
+
+    json_dict = decode_json_config_file_to_dict(filepath=filepath)
+    if not isinstance(json_dict, dict):
+        print('ERROR: get_single_variable_from_json_file(): The file is not a correctly structured json file')
+        return None
+    else:
+        if arg_name in json_dict:
+            return json_dict[arg_name]
+        else:
+            print('ERROR: get_single_variable_from_json_file(): The key "{}" does not exists in dictionary from the json file'.format(arg_name))
+            return None
+
+    return None
 
 # Overwrite is not true, will skip if it's the case. And will infer file type. So make sure to_filename is only the name of the file, don't include the extension
-def download_image_from_url(url_image, to_filename_with_no_extension):
+def download_image_from_url(url_image, to_filename_with_no_extension, to_dir_path):
     try:
         response = requests.get(url_image, stream=True)
     except Exception as e:
@@ -82,21 +122,29 @@ def scrap_for_current_image_link_and_title(url_sub_profile):
 
     return image_link, title
 
-# only download highest quality of the public downloadable images
-def download_all_image_from_profile_link(url_profile):
-    print()
+# Return a list of art links for an artist and the artist name
+def scrap_for_all_art_link_from_profile_link(url_profile):
+
+
+    return artist_name, art_url_list
 
 def main():
+    config_path = r'./config.json'
+
+    download_dir = get_single_variable_from_json_file( config_path, "download_dir" )
+    print(download_dir)
+
     url_list = [
         r'https://www.deviantart.com/arsenixc/art/Wentmon-845001018',
         r'https://www.deviantart.com/arsenixc/art/Imperial-city-839848270',
+        r'https://www.deviantart.com/arsenixc/art/Arvez-and-Arinly-444904429',
     ]
 
     print( ' Downloading for artist {}'.format("arsenixc") )
     for url in url_list:
         image_link, image_title = scrap_for_current_image_link_and_title(url)
 
-        download_image_from_url(url_image=image_link, to_filename_with_no_extension=image_title)
+        download_image_from_url(url_image=image_link, to_filename_with_no_extension=image_title, to_dir_path=download_dir)
 
 
 if __name__ == '__main__':
